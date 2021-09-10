41cfbc095b8f1ec2b6fd6d86a1c3c4c3d5c2bc39
eude313 <eudes.charles@student.moringaschool.com>
2021-09-10 17:40:12 +0300

    add commit

--- Binsta/settings.py
+++ Binsta/settings.py
@@ -78,7 +78,10 @@ WSGI_APPLICATION = 'Binsta.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'insta',
+        'USER': 'postgres',
+        'PASSWORD': config('PASSWORD'),
+        'HOST':'localhost',
     }
 }
 

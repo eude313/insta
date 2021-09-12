b9a549e23b089e525482b017031ea58151497be1
eude313 <eudes.charles@student.moringaschool.com>
2021-09-12 18:42:10 +0300

    add commit

--- insta/views.py
+++ insta/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -6,7 +7,9 @@ def login(request):
     return render(request, 'auth/login.html')
 
 def signin(request):
-    return render(request, 'auth/signin.html')
+    form = UserCreationForm(request.POST)
+    context= { 'form': form }
+    return render(request, 'auth/signin.html', context)
 
 
 def home(request):
